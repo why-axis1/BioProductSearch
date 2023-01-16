6939c9bfe5123cb9aecd354e6b2aa795c2820613
why-axis1 <122762902+why-axis1@users.noreply.github.com>
2023-01-15 23:32:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import difflib
+
+
+app = Flask(__name__)
+CORS(app)
+
+class Product:
+    def __init__(self, name, price, category, keywords):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.keywords = keywords
+
+products = [
+    Product("Soap Dispenser", 16.90, "Kitchen", ["kitchen", "soap", "cleaning", "kitchen gadget"]),
+    Product("Expandable Food Container", 5, "Kitchen", ["expandable food container", "expandable", "kitchen", "food", "organize", "container", "storage"]),
+    Product("Outlet timer", 18.50, "Electronics", ["electronics", "outlet timer", "tech", "gadget", "time"]),
+]
+
+@app.route('/search', methods=['POST'])
+def search_products():
+    data = request.get_json()
+    keyword = data.get('keyword')
+    results = []
+    if keyword.isnumeric():
+        for product in products:
+            if abs(int(keyword) - product.price) <= 5:
+                results.append({'name': product.name, 'price': product.price, 'category': product.category})
+    else:
+        product_matches = []
+        for product in products:
+            product_matches.extend([product.name] + product.keywords)
+        close_matches = difflib.get_close_matches(keyword, product_matches, n=1, cutoff=0.6)
+        if close_matches:
+            for product in products:
+                if close_matches[0] in [product.name] + product.keywords:
+                    results.append({'name': product.name, 'price': product.price, 'category': product.category})
+    return jsonify(results)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
